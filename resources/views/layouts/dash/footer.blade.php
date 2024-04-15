d2df967e0fab955d7a996de89906dadb4f706acf
Irham Rizqan Zakiy <irhamrizkan@gmail.com>
2024-04-15 15:34:21 +0700

    First Commit

---
+++ resources/views/layouts/dash/footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2023-2024 <a href="https://www.instagram.com/padasuka.cafe/">Padasuka Cafe</a>.</strong>
+    All rights reserved.
+</footer>
